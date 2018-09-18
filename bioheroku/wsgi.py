e631423a9329e17c5717a57dc192fea34bb0b52e
ryota <ryota@macbook-pro.attlocal.net>
2018-09-17 21:06:55 -0700

    add allowhowst in wsgi file

--- bioheroku/wsgi.py
+++ bioheroku/wsgi.py
@@ -15,4 +15,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bioheroku.settings')
 
 application = get_wsgi_application()
 
-#ALLOWED_HOSTS = ['herokudjangoapp777.herokuapp.com']
+ALLOWED_HOSTS = ['herokudjangoapp777.herokuapp.com']
