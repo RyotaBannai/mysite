0e8b25b47af82478381d13579e12d72a38968dec
ryota <ryota@macbook-pro.attlocal.net>
2018-09-17 21:31:29 -0700

    change the static path

--- bioheroku/settings.py
+++ bioheroku/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
+STATIC_URL = os.path.join(PROJECT_ROOT, 'static')
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
