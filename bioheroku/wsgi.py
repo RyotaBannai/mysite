732b047e469829f20a3224828300e11828f8377f
ryota <ryota@macbook-pro.attlocal.net>
2018-09-17 22:06:54 -0700

    test allow_host multiple set, arrow

--- bioheroku/wsgi.py
+++ bioheroku/wsgi.py
@@ -14,6 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bioheroku.settings')
 
 application = get_wsgi_application()
-
-ALLOWED_HOSTS = ['localhost']
-ALLOWED_HOSTS = ['herokudjangoapp777.herokuapp.com']
