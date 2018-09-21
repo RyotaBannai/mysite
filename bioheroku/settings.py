84a40abe4b3483fa5aab1d75cc3ca7782c11090d
RyotaBannai <ryotabannai0528@gmail.com>
2018-09-21 16:47:48 -0700

    add django-heroku to make it work postgresql

--- bioheroku/settings.py
+++ bioheroku/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url 
+import django_heroku
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -80,6 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    #'default': dj_database_url.config(default='postgres://localhost:5431/ryota')
 }
 
 
@@ -135,7 +140,6 @@ STATICFILES_DIRS = (
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
