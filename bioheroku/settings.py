b4e40fd36d70c6476471e316aa616e81680d9bfa
RyotaBannai <ryotabannai0528@gmail.com>
2018-09-21 18:06:52 -0700

    setting for heroku use posgres

--- bioheroku/settings.py
+++ bioheroku/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'bioheroku.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {'default': { 'ENGINE': 'django.db.backends.sqlite3', 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), }}
+#DATABASES = {'default': { 'ENGINE': 'django.db.backends.sqlite3', 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), }}
 
 ENVIRONMENT = 'production'
 DATABASES['default'] = dj_database_url.config(
