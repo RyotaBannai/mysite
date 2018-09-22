6e16c3706bd5f693fe88503488cf687dfffdbce7
RyotaBannai <ryotabannai0528@gmail.com>
2018-09-21 18:30:43 -0700

    setting for heroku use posgres

--- bioheroku/settings.py
+++ bioheroku/settings.py
@@ -79,13 +79,8 @@ WSGI_APPLICATION = 'bioheroku.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-#DATABASES = {'default': { 'ENGINE': 'django.db.backends.sqlite3', 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), }}
-DATABASES = {'default': { 'ENGINE': 'django.db.backends.postgresql_psycopg2', 
-'NAME': dj_database_url.config(default='postgresql-colorful-58370'), }
-}
-#ENVIRONMENT = 'production'
-#DATABASES = {'default': dj_database_url.config(default='postgresql-colorful-58370')}
-#DATABASES = { 'ENGINE': dj_database_url.config(), 'NAME': 'postgresql-colorful-58370', }
+DATABASES = {'default': { 'ENGINE': 'django.db.backends.sqlite3', 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), }}
+#DATABASES = {'default': { 'ENGINE': 'django.db.backends.postgresql_psycopg2', 'NAME': dj_database_url.config(default='postgresql-colorful-58370'), }}
 
 
 # Password validation
